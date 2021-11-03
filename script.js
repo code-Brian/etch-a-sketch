fd977487115720e6425212ed8cb4a2e4af693515
Brian Denmark <testingarea479@gmail.com>
2021-11-03 11:31:47 -0700

    updated usability and featurs to be smooth, conistent and fill the entire space of the container

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
-const container = document.getElementById("container");
-let rows = document.getElementsByClassName("gridRow");
-let cells = document.getElementsByClassName("cell");
+const container = document.getElementById('container');
+let rows = document.getElementsByClassName('gridRow');
+let cells = document.getElementsByClassName('cell');
 
-function defaultGrid() {
-    makeRows(16);
-    makeColumns(16);
-}
-
-function makeRows(rowNum) {
-    for (r = 0; r < rowNum; r++) {
+function makeRows (rowNum){
+    for(r = 0; r < rowNum; r++){
         let row = document.createElement('div');
-        container.appendChild(row).className = "gridRow";
-    };
-}
-
-function makeColumns(cellNum) {
-    for(i = 0; i < rows.length; i++){
-        for (j=0; j < cellNum; j++) {
-            let newCell = document.createElement("div");
-            rows[j].appendChild(newCell).className = "cell";
-        }
+        container.appendChild(row).className = 'gridRow';
     }
 }
 
-defaultGrid();
+function makeColumns (cellNum){
+    for(i = 0; i < rows.length; i++){
+        for(j = 0; j < cellNum; j++){
+            let newCell = document.createElement('div');
+            rows[j].appendChild(newCell).className = 'cell';
+        }
+    }      
+}
+
+
+function defaultGrid(rowNum,colNum){
+    makeRows(rowNum);
+    makeColumns(colNum);
+}
+defaultGrid(100,100);
+
+
 
 const cellGrid = document.querySelectorAll('div.cell');
 
 cellGrid.forEach((div) => {
-    div.addEventListener("mouseover", ( event ) => {
-        event.target.style.backgroundColor = "black";
+    div.addEventListener("mouseover",(event) => {
+        event.target.style.backgroundColor = 'black';
         console.log(event);
     });
 });
-
