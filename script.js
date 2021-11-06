62c22e9482e12137583a69d28eb054179adfa6b1
Brian Denmark <testingarea479@gmail.com>
2021-11-06 16:37:00 -0700

    added rainbow feature

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const container = document.getElementById('container');
+const colorButtons = document.querySelectorAll('.color-choice');
 let slider = document.querySelector('#gridSlider');
+let color = "black";
 
 
 // functions
@@ -12,7 +14,7 @@ function createGrid(gridNumber) {
         container.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
         container.insertAdjacentElement('beforeend', pixel);
     }
-    let pixels = document.querySelectorAll('.gridPixel');
+    let pixels = container.querySelectorAll('div');
     pixels.forEach(pixel => pixel.addEventListener('mouseover', changeColor));
 }
 
@@ -23,11 +25,40 @@ function updateGrid(gridNumber) {
 }
 
 function changeColor() {
-    let pixelColor = container.querySelectorAll('.gridPixel');
-    this.style.backgroundColor = 'black';
+    switch(color) {
+        case'gray':
+            this.style.backgroundColor = 'rgba(0,0,0, 0.1)';
+            console.log("gray passed thru changeColor");
+            break;
+        case'rainbow':
+            this.style.backgroundColor = `hsl(${Math.random() * 360} 100% 50%)`;
+            console.log(`${color} pass thru rainbow case`);
+            break;
+        default:
+            this.style.backgroundColor = color;
+            console.log(`${color} pass thru default case`);
+            break;
+    }
+}
+
+function colorChoice(event) {
+    switch(event.target.dataset.color) {
+        case'gray':
+            color = 'gray';
+            console.log(color);
+            break;
+        case'rainbow':
+            color = 'rainbow';
+            console.log(color);
+            break;
+        default:
+            color = 'black';
+            break;
+    }
 }
 
 createGrid(10);
 
 // listeners
 slider.addEventListener('mouseup', updateGrid);
+colorButtons.forEach(colorButton => colorButton.addEventListener('click', colorChoice));
